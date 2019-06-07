f5ed76a5dbd5de70514203afd9baf0a67bc5cbb3
karthikeyan <imkarthikeyans@gmail.com>
2019-06-07 13:37:23 +0530

    Added the broadcast date and title

--- app.js
+++ app.js
@@ -3,28 +3,25 @@ const puppeteer = require('puppeteer');
 
 const LCWTM_url = "https://www.podbean.com/podcast-detail/jh5a4-3fe34/Learn-to-Code-With-Me-Podcast";
 const JSjabber_url = "https://www.podbean.com/podcast-detail/d4un8-57595/JavaScript-Jabber-Podcast";
-
-const run = async () => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto(`${LCWTM_url}`);
-      let episodeLinks = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('span.datetime')).map((item) => ({
-            url:  item.baseURI,
-            text: item.innerText
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    //await page.setExtraHTTPHeaders({Referer: 'https://sparktoro.com/'}) 
+    await page.goto(`${JSjabber_url}`);
+    await page.waitForSelector('td > a.title.listen-now');
+  
+          let episodeLinks = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('td > a.title.listen-now')).map((item) => ({
+            url:  item.getAttribute('href'),
+            text: item.innerText,
+            Broadcast_date : item.nextElementSibling.innerText
           })
         );
       });
-      browser.close();
-      return resolve(episodeLinks);
-    } catch (e) {
-      return reject(e);
-    }
-  })
-}
+  
+        console.log(episodeLinks);
+      await browser.close();
+      })();
 
-run()
-.then(console.log)
-.catch(console.error);
+      
+  
